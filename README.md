61db9ad6758e499b8c96bd2a76c668b46397a6a0
Amritanshu Prasad <amri@imsc.res.in>
2017-08-21 12:12:16 +0530

    added README.md and code.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 20170821
 Homework assigned on 21st August 2017
+
+1. Install Sage on your computer(s)
+1. Complete the code in code.py
+1. For help, you may watch the tutorials at https://www.imsc.res.in/~amri/sagecourse/
---
+++ code.py
@@ -0,0 +1,35 @@
+# Create a list with the first ten triangular numbers
+# (see https://oeis.org/A000217)
+
+L = [ for i in range(10)]
+
+# Create a function to test if a number is prime
+def is_prime(n):
+    """
+    Test if ``n`` is a prime.
+    """
+
+# Tests
+# is_prime(2033) == False
+# is_prime(2039) == True
+
+# Create a function which returns a list of the first ten prime numbers
+# greater than or equal to n
+
+def next_ten_primes(n):
+    """
+    Return the list of the first ten prime numbers greate than or equal to n
+
+    
+    """
+
+
+# Tests
+# next_ten_primes(2033) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
+# next_ten_primes(2039) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
+
+
+
+
+
+
